38ee48439e2925a211737de7ac3b7cf0e6d34995
dwd <wenyi173@gmail.com>
2024-11-23 19:35:25 +0800

    add func: merge_agg_trades_to_klines

---
+++ diy_klines.py
@@ -0,0 +1,12 @@
+from enums import SymbolType
+
+
+def merge_agg_trades_to_klines(
+        syb_type: SymbolType,
+        symbol: str,
+        agg_trades_dir: str,
+        klines_dir: str,
+        start_agg_trade_file_name: str = ""
+        ) -> None:
+    pass
+    
