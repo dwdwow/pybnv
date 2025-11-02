01671c466a485ac5ad71949ed5c0ac6f81d76d50
dwd <wenyi173@gmail.com>
2025-11-02 19:03:22 +0800

    update

---
+++ run/all_klines.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).parent.parent.resolve()))
+
+
+import asyncio
+from cex.bnc import public
+from pybnv.klines import download, SymbolType  # pyright: ignore[reportMissingImports]
+
+
+async def main():
+    spsybs = await public.get_spot_symbols()
+    umsybs = await public.get_um_symbols()
+    cmsybs = await public.get_cm_symbols()
+    
+    sps = []
+    for syb in spsybs:
+        sps.append(syb.symbol)
+    
+    ums = []
+    for syb in umsybs:
+        ums.append(syb.symbol)
+    
+    cms = []
+    for syb in cmsybs:
+        cms.append(syb.symbol)
+        
+
+    intervals = ["1s", "1m", "5m", "15m", "30m", "1h", "2h", "4h", "12h"]
+    for symbol in sps:
+        for interval in intervals:
+            download(
+                SymbolType.SPOT, symbol, interval,
+            )
+            
+    
+    intervals = intervals[1:]
+    for symbol in ums:
+        for interval in intervals:
+            download(
+                SymbolType.FUTURES_UM, symbol, interval,
+            )
+    
+    for symbol in cms:
+        for interval in intervals:
+            download(
+                SymbolType.FUTURES_CM, symbol, interval,
+            )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
